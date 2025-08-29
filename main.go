054e1aac016bd6cd6744189e750dda051f1a9331
Hadi Chokr <hadichokr@icloud.com>
2025-08-29 09:19:20 +0300

    Upsie wrong comment format

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
-# SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
-# SPDX-FileCopyrightText: 2025 Hadi Chokr <hadichokr@icloud.com>
+// SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+// SPDX-FileCopyrightText: 2025 Hadi Chokr <hadichokr@icloud.com>
 
 package main
 
